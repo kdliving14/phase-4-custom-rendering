74a0e08efd6bef8ecce52e651bb4604de454d62f
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-08-29 14:47:29 -0700

    commit tree db606d07744e3a8abfcb7bb93b2c194ff21128a3

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,5 +1,20 @@
 class Cheese < ApplicationRecord
   
+  def index
+    cheeses = Cheese.all
+    # render  json: cheeses, only: [:id, :name, :price, :is_best_seller]
+    render json: cheeses, except: [:created_at, :updated_at]
+  end
+
+  def show
+    cheese = cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {error: 'Cheese not found'}, status: :not_found
+    end
+  end
+
   def summary
     "#{name}: $#{price}"
   end
